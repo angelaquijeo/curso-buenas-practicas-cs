671e9ee4a439af087ca0dbb5dd7ac32298aac4c8
Angel Aquije <angel.aquije.96@gmail.com>
2023-06-04 23:37:41 -0500

    Se ha usado el try catch para controlar las excepciones, tambien se aprendio que si puedes manejar los errores antes que lleguen al catch es mejor para la optimizacion del codigo. Se usaron estos conocimientos para optimizar los try catch y se manejaron los posibles errores antes de que lleguen al catch. Tambien se modificaron algunos nombres de algunas variables para su mejor reconocimiento en el codigo

--- Program.cs
+++ Program.cs
@@ -42,8 +42,8 @@ namespace ToDo
             Console.WriteLine("4. Salir");
 
             // Read line
-            string line = Console.ReadLine();
-            return Convert.ToInt32(line);
+            string numberTask = Console.ReadLine();
+            return Convert.ToInt32(numberTask);
         }
 
         public static void ShowMenuRemove()
@@ -54,18 +54,33 @@ namespace ToDo
                 // Show current taks
                 TaskListMethod();
 
-                string line = Console.ReadLine();
+                string numberTaskRemove = Console.ReadLine();
                 // Remove one position
-                int indexToRemove = Convert.ToInt32(line) - 1;
+                int indexToRemove = Convert.ToInt32(numberTaskRemove) - 1;
+
+                if (indexToRemove>(TaskList.Count - 1) || indexToRemove < 0)
+                Console.WriteLine("Numero de tarea seleccionado no es valido");
+                else {
+                    if (indexToRemove > -1 && TaskList.Count > 0)
+                    {
+                        string taskRemoved = TaskList[indexToRemove];
+                        TaskList.RemoveAt(indexToRemove);
+                        Console.WriteLine("Tarea " + taskRemoved + " eliminada");
+                    }
+                }
+                /*ESTE CODIGO LO HICE YO, pero me manda al catch cuando no es correcto el numero
                 if (indexToRemove > -1 && TaskList.Count > 0)
                 {
                     string task = TaskList[indexToRemove];
                     TaskList.RemoveAt(indexToRemove);
                     Console.WriteLine("Tarea " + task + " eliminada");
-                }
+                } else {
+                    Console.WriteLine("Numero de tarea seleccionado no es valido");
+                }*/
             }
             catch (Exception)
             {
+                Console.WriteLine("Ha ocurrido un error al eliminar la tarea");
             }
         }
 
@@ -74,12 +89,18 @@ namespace ToDo
             try
             {
                 Console.WriteLine("Ingrese el nombre de la tarea: ");
-                string task = Console.ReadLine();
-                TaskList.Add(task);
-                Console.WriteLine("Tarea registrada");
+                string taskEnteredByUser = Console.ReadLine();
+                if((string.IsNullOrEmpty(taskEnteredByUser))==true)
+                    Console.WriteLine("Se requiere que ingrese una tarea");
+                else
+                {
+                    TaskList.Add(taskEnteredByUser);
+                    Console.WriteLine("Tarea registrada");
+                }
             }
             catch (Exception)
             {
+                Console.WriteLine("Ha ocurrido un error al intentar escribir la tarea");
             }
         }
 
